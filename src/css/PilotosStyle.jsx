eebd0717f05380b0af992dde7ea817ce7ee35167
Fernando C <thelonder461@gmail.com>
2024-09-20 21:25:38 -0300

    Seção Pilotos finalizada.

--- src/css/PilotosStyle.jsx
+++ src/css/PilotosStyle.jsx
@@ -1,3 +1,198 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const PilotosStyle = styled.section`` 
+export const PilotosStyle = styled.section`
+  #header {
+    background-color: black;
+  }
+
+  .nossos_carros {
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #car-selector {
+    position: relative;
+    display: flex;
+    align-items: center;
+  }
+
+  #car-selector .passos {
+    font-size: 50px;
+    margin: 10px;
+    background: none;
+    border: none;
+    cursor: pointer;
+    color: #dd052b;
+    transition: all 0.3s;
+  }
+
+  #car-selector .passos:hover {
+    color: #af0423;
+  }
+
+  #car-selector #carImage {
+    width: 450px;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  .nossos_carros h2 {
+    margin-top: 80px;
+    font-size: 50px;
+  }
+
+  .nossos_carros p {
+    font-size: 20px;
+    margin-bottom: 90px;
+  }
+
+  .nossos_carros {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh; /* Ajuste conforme necessário */
+  }
+
+  #car-selector {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px; /* Espaço entre os itens */
+  }
+
+  .nossos_carros .selecao {
+    font-size: 20px;
+    margin: 30px;
+    background: none;
+    border: none;
+    cursor: pointer;
+    color: #ffffff;
+    background-color: #dd052b;
+    transition: all 0.3s;
+    padding: 15px;
+    border-radius: 10px;
+    text-align: center;
+  }
+
+  .nossos_carros .selecao:hover {
+    background-color: #af0423;
+    color: #e7e7e7;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  }
+
+  @media screen and (max-width: 600px) {
+    .nossos_carros h2 {
+      font-size: 35px;
+    }
+    .nossos_carros p {
+      font-size: 20px;
+      margin: 10px;
+    }
+    #car-selector img {
+      width: 250px;
+    }
+    .nossos_carros .selecao {
+      font-size: 15px;
+    }
+    #car-selector .passos {
+      font-size: 30px;
+      margin: 0;
+    }
+  }
+
+  #backToTop {
+    visibility: hidden; /* Hidden by default */
+    opacity: 0;
+    transition: visibility 0s 0.3s, opacity 0.3s linear, all 0.3s; /* Transition effect */
+    position: fixed; /* Fixed/sticky position */
+    bottom: 20px; /* Place the button at the bottom of the page */
+    right: 30px; /* Place the button 30px from the right */
+    z-index: 99; /* Make sure it does not overlap */
+    border: none; /* Remove borders */
+    outline: none; /* Remove outline */
+    color: #0e0e0e; /* Text color */
+    background: none;
+    cursor: pointer; /* Add a mouse pointer on hover */
+    border-radius: 10px; /* Rounded corners */
+    font-size: 45px; /* Increase font size */
+  }
+
+  #backToTop.show {
+    visibility: visible;
+    opacity: 1;
+    transition: visibility 0s, opacity 0.5s linear, all 0.3s;
+  }
+
+  #backToTop:hover {
+    color: #dd052b; /* Add a dark-grey background on hover */
+  }
+
+  @media screen and (max-width: 600px) {
+    #backToTop {
+      bottom: 20px; /* Place the button at the bottom of the page */
+      right: 10px; /* Place the button 30px from the right */
+    }
+  }
+
+  .bi-arrow-right-circle-fill {
+    font-size: 30px;
+  }
+
+  .bi-arrow-left-circle-fill {
+    font-size: 30px;
+  }
+
+  .bi-arrow-up-circle-fill {
+    font-size: 30px;
+  }
+
+  
+  .info-button {
+    font-size: 1.8rem;
+    background-color: #af0423;
+    border-radius: 50px;
+    margin: 5% 0;
+    transition: transform 0.3s ease;
+    color: #fff;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  
+  #pilotInfo {
+    position: absolute;
+    bottom: -60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    border: 2px solid black;
+    border-radius: 10px;
+    opacity: 0;
+    animation: scale-display .5s forwards;
+  }
+
+  .hidden {
+    display: none;
+  }
+
+  @keyframes scale-display {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .lower {
+    transform: translateY(200%);
+  }
+
+
+  .button-pilotos {
+    text-align: center;
+  }
+`;
--- src/routes/Pilotos.jsx
+++ src/routes/Pilotos.jsx
@@ -1,8 +1,85 @@
+import { PilotosStyle } from "../css/PilotosStyle"
+import { useState } from "react";
+
 const Pilotos = () => {
-    return(
-        <>
-        </>
-    )
+
+  const cars = [
+    { image: 'src/assets/img/home/secao/piloto1.png', infoImage: 'src/assets/img/home/secao/info1.png', info: 'Piloto 1: Informações' },
+    { image: 'src/assets/img/home/secao/piloto2.png', infoImage: 'src/assets/img/home/secao/info2.png', info: 'Piloto 2: Informações' },
+    { image: 'src/assets/img/home/secao/piloto3.png', infoImage: 'src/assets/img/home/secao/info3.png', info: 'Piloto 3: Informações' },
+    { image: 'src/assets/img/home/secao/piloto4.png', infoImage: 'src/assets/img/home/secao/info4.png', info: 'Piloto 4: Informações' },
+    { image: 'src/assets/img/home/secao/piloto5.png', infoImage: 'src/assets/img/home/secao/info5.png', info: 'Piloto 5: Informações' },
+    { image: 'src/assets/img/home/secao/piloto6.png', infoImage: 'src/assets/img/home/secao/info6.png', info: 'Piloto 6: Informações' },
+  ];
+
+  const [currentCarIndex, setCurrentCarIndex] = useState(0);
+  const [showInfo, setShowInfo] = useState(false);
+
+  // Função para avançar para o próximo piloto
+  const nextCar = () => {
+    setCurrentCarIndex((currentCarIndex + 1) % cars.length);
+  };
+
+  // Função para voltar para o piloto anterior
+  const previousCar = () => {
+    setCurrentCarIndex((currentCarIndex - 1 + cars.length) % cars.length);
+  };
+
+  // Função para mostrar ou esconder informações do piloto
+  const showPilotInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
+  const showInfoToggle = showInfo
+    ? 'info-piloto'
+    : 'info-piloto hidden'
+
+  const buttonClassName = showInfo
+    ? 'info-button lower'
+    : 'info-button'
+
+  // Função para rolar a página para o topo
+  const topFunction = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+
+  return(
+    <PilotosStyle>
+      <section id="pagina-pilotos">
+
+        <div className="nossos_carros">
+
+          <h2>Conheça alguns pilotos:</h2>
+          <p>Venha saber mais sobre os pilotos da Fórmula E</p>
+
+          <div id="car-selector"> 
+            <button className="passos" onClick={previousCar}><i className="bi-arrow-left-circle-fill"></i></button>
+            <img src={cars[currentCarIndex].image} id="carImage" alt="Imagem do Carro" className="loaded" />
+
+            <div className={showInfoToggle} id="pilotInfo">
+              <img src={cars[currentCarIndex].infoImage} id="pilotInfoImage" alt="Imagem de Informações" />
+              <div id="pilotInfoContent">{cars[currentCarIndex].info}</div>
+            </div>
+
+            <button className="passos" onClick={nextCar}><i className="bi-arrow-right-circle-fill"></i></button>
+          </div>
+            
+        </div>
+              
+          <br/>
+          <br/>
+
+        <div className="button-pilotos">
+          <button className={buttonClassName} onClick={showPilotInfo}>Saiba mais sobre o piloto</button>
+        </div>
+    
+      </section>
+
+      <button onClick={topFunction} id="backToTop" title="Go to top"><i className="bi-arrow-up-circle-fill"></i></button>
+      
+    </PilotosStyle>
+  )
 }
 
 export default Pilotos
